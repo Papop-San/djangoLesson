791d787498a9f1e0680c741af391b5838d6b8151
papop-san <008khun159@gmail.com>
2024-09-26 11:11:06 +0700

    Api_Crud

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_user(request):
+    users = User.objects.all()  # Get all users
+    serializer = UserSerializer(users, many=True)  # Serialize the list of users
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
