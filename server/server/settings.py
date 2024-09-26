861edf5f2fa384395c4acce6f279def3564c1675
papop-san <008khun159@gmail.com>
2024-09-26 10:04:02 +0700

    update_directory

--- server/server/settings.py
+++ server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f&v2zm$(0m-dkv7#1h%s2av^i$#0lil7=+_g#!!6mz1!r(yfc7'
+SECRET_KEY = 'django-insecure-a0@7g=8i=yco=kej()%6+q0@-$8%*ysrag!-45%1kex70^&5u!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
